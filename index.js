1f2979fe7c94a523fa1b7d01cf5757e69e36c5aa
Neo <johnsson.neo@gmail.com>
2026-02-26 14:25:03 +0100

    lade till dagar och månader

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 
 const display = document.querySelector("#myDiv h1");
+const daysDisplay = document.querySelector("#days h1:nth-of-type(2)");
+const monthsDisplay = document.querySelector("#days h1:nth-of-type(1)");
 
 let time = null;
 let startTime = 0;
@@ -30,13 +32,16 @@ function pad(number)  {
 function updateTimer() {
     const currrentTime = Date.now();
     elapsedTime = currrentTime - startTime;
-    const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    const months = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 30));
+    const days = Math.floor((elapsedTime % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((elapsedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
     const milliseconds = Math.floor((elapsedTime % 1000) / 10);
 
-    display.textContent = `${pad(days)}:${pad(hours)}:${pad(minutes)}:${pad(seconds)}:${pad(milliseconds)}`;
+    monthsDisplay.textContent = `${months} Months`;
+    daysDisplay.textContent = `${days} Days`;
+    display.textContent = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}:${pad(milliseconds)}`;
     time = requestAnimationFrame(updateTimer);
 }
 
